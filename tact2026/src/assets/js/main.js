c27aef397128a9f950b6dc565b12eb1fa4693af6
fifaliana andriana <andrianjafyfifaliana@gmail.com>
2026-02-18 14:46:23 +0300

    page d'accueil

--- tact2026/src/assets/js/main.js
+++ tact2026/src/assets/js/main.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const body= document.querySelector("body");
-    if (body.style.backgroundColor=="var(--bgc_dark)") {
-        body.style.color=="var(--color_dark)";
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.querySelector("body");
+    const menuResponsive = document.querySelector(".menuResponsive");
+    const menuStyle = document.querySelector(".menuStyle");
+    const ParentMenuRspons = document.querySelector(".ParentMenuRspons");
+    const actif = document.querySelector(".actif");
+
+    if (body.style.backgroundColor === "var(--bgc_dark)") {
+        body.style.color = "var(--color_dark)";
+    } else {
+        body.style.color = "var(--color_light)";
     }
-    else{
-        body.style.color=="var(--color_light)"
-    }
+
+    menuResponsive.addEventListener("click", () => {
+        ParentMenuRspons.style.display = "flex";
+    });
 });

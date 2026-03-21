65e6d226608449ef7dd6ad47478f249c9e221982
Fifaliana <andrianjafyfifaliana@gmail.com>
2026-03-21 08:54:57 +0300

    menu responsive

--- tact2026/src/components/navbar.js
+++ tact2026/src/components/navbar.js
@@ -1,45 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    const menuContainer = document.querySelector(".menuContainer");
-    const menuResponsive = document.querySelector(".menuResponsive");
+    const body=document.querySelector("body");
+    const navBar=document.querySelector("#navBar");
+    const menuContainer=document.querySelector(".menuContainer");
+    const menuStyle = document.querySelector(".menuStyle");
+    const menuResponsive = document.querySelector(".menuResponsive")
     const ParentMenuRspons = document.querySelector(".ParentMenuRspons");
-    const links=document.querySelectorAll(".menuStyle a")
+    const actif=document.querySelector("a");
 
-    /* menu actif */    
-    links.forEach(link=>{        
-        link.addEventListener("click",()=>{
-            links.forEach(l=>l.classList.remove("actif"))            
-            link.classList.add("actif")        
-        })        
-    })
-    
-    function checkScreen() {
-
-        if (window.innerWidth >= 768) {
-            menuResponsive.style.display = "none";
-            ParentMenuRspons.style.display = "none";
-            menuContainer.style.display = "flex";
-
-        } else {
-            menuResponsive.style.display = "flex";
-            ParentMenuRspons.style.display = "none";
-
-        }
-
-    }
-
-    checkScreen();
-
-    window.addEventListener("resize", checkScreen);
-
-    menuResponsive.addEventListener("click", () => {
-
-        if (ParentMenuRspons.style.display === "flex") {
-            ParentMenuRspons.style.display = "none";
-        } else {
-            ParentMenuRspons.style.display = "flex";
-        }
-
+    /*affichage des menus en responsive*/
+    menuResponsive.addEventListener('click',()=>{
+        ParentMenuRspons.classList.toggle('active');
     });
-
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 768) {
+            ParentMenuRspons.classList.remove('active');
+        }
+    });
 });
